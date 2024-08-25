d278f2d9a97d17b640c204bc517099d14603e61b
c4710n <c4710n@users.noreply.github.com>
2024-08-25 21:41:14 +0800

    release: bump v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule RFC2822.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "An implementation of RFC 2822."
   @source_url "https://github.com/cozy-elixir/rfc2822"
 
